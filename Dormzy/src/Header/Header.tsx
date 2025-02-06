e89ca63c375a34912cc70a959c976332413c2dd3
Juny7736 <handaniel721@gmail.com>
2025-02-05 22:56:17 -0500

    added different fonts and colours

--- Dormzy/src/Header/Header.tsx
+++ Dormzy/src/Header/Header.tsx
@@ -1,22 +1,12 @@
+import NavigationMenu from "@/NavigationMenu/NavigationMenu";
 import DropMenu from "@/DropMenu/DropMenu";
+
 function Header() {
   return (
     <header className="sticky-header">
       <div className="header-content">
         <div className="titleName">Dormzy</div>
-        <nav>
-          <ul>
-            <li>
-              <a href="#home">Main</a>
-            </li>
-            <li>
-              <a href="#about">Houses</a>
-            </li>
-            <li>
-              <a href="#services">Explore</a>
-            </li>
-          </ul>
-        </nav>
+        <NavigationMenu />
         <DropMenu />
       </div>
     </header>
